abe7df33b0e14b1e0dba8c491ca843131194e208
Zou Wei <zwcloud@hotmail.com>
2022-02-01 14:12:25 +0800

    add working WebAssembly project based on NativeAOT-LLVM WebAssembly
    from https://github.com/dotnet/runtimelab/tree/feature/NativeAOT-LLVM

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from C# Main");
+        }
+
+        static int Func()
+        {
+            Console.WriteLine("Hello World from C# Func");
+            return 44168;
+        }
+    }
+}
